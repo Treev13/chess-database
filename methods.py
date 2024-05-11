e30cb593298b0286ef4ca16a058f68e809e2b9e7
Csaba Katona <115178933+Treev13@users.noreply.github.com>
2024-05-11 21:43:10 +0200

    change how to add ratings to matches

--- methods.py
+++ methods.py
@@ -41,24 +41,43 @@ def get_rating (period, fide_id):
         cursor.close()
     return rating
 
-def format_matches(name, id, data, cursor):
-    mod_data = []
+def add_rating_and_fed_to_matches(id, data):
+    final_data = dict()
+    final_data['matches'] = []
+    event = get_event_by_id(id)
     periods = get_rating_list_periods()
-    event = get_event_by_id(id, cursor)
     period = check_latest_rating_list(event['start_date'], periods)
+    final_data['event_id'] = event['event_id']
+    final_data['event_name'] = event['event_name']
+    final_data['p_name'] = data[0]['w_name']
+    final_data['p_year'] = data[0]['w_year']
+    final_data['p_month'] = data[0]['w_month']
+    player_id = data[0]['w_fide_id']
+    final_data['p_rating'] = (get_rating(period, player_id))['rating']
+    final_data['p_nat'] = (get_rating(period, player_id))['fed']
     for row in data:
-        data_from_ratings_for_white = (get_rating(period, row['w_fide_id']))
+        match = dict()
+        match['date'] = row['date']
+        match['round'] = row['round']
+        match['color'] = row['color']
+        match['o_name'] = row['b_name']
         data_from_ratings_for_black = (get_rating(period, row['b_fide_id']))
-        row['w_rating'] = data_from_ratings_for_white['rating']
-        row['w_nat'] = data_from_ratings_for_white['fed']
-        row['b_rating'] = data_from_ratings_for_black['rating']
-        row['b_nat'] = data_from_ratings_for_black['fed']
-        if row['w_name'] != name:
+        match['o_rating'] = data_from_ratings_for_black['rating']
+        match['o_nat'] = data_from_ratings_for_black['fed']
+        match['o_year'] = row['b_year']
+        match['o_month'] = row['b_month']
+        match['result'] = row['result']
+        match['rat_change'] = (calculate_fide(final_data['p_rating'], match['o_rating'], row['result']))
+        final_data['matches'].append(match)
+    
+    return final_data
 
+def format_matches(name, data):
+    mod_data = []
+    for row in data:
+        if row['w_name'] != name:
             row['w_fide_id'], row['b_fide_id'] = row['b_fide_id'], row['w_fide_id']
             row['w_name'], row['b_name'] = row['b_name'], row['w_name']
-            row['w_nat'], row['b_nat'] = row['b_nat'], row['w_nat']
-            row['w_rating'], row['b_rating'] = row['b_rating'], row['w_rating']
             row['w_year'], row['b_year'] = row['b_year'], row['w_year']
             row['w_month'], row['b_month'] = row['b_month'], row['w_month']
             
@@ -67,11 +86,8 @@ def format_matches(name, id, data, cursor):
             elif row['result'] == '0-1':
                 row['result'] = '1-0'
             row['color'] = ('b')
-            
         else: row['color'] = ('w')
-        row['rat_change'] = (calculate_fide(row['w_rating'], row['b_rating'], row['result']))
-        row['event_id'] = event['event_id']
-        row['event_name'] = event['event_name']
+        
         mod_data.append(row)
     return mod_data
 
@@ -93,13 +109,18 @@ def calculate_stats(matches):
     return {'points': points, 'matches': len(matches)}
 
 def get_matches_by_player_on_event(name, id, cursor):
-    cursor.execute(MATCHES_BY_PLAYER_AND_EVENT,(name, name, id)) 
-    data = cursor.fetchall()
-    return format_matches(name, id, data, cursor)
+    with connection:
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(MATCHES_BY_PLAYER_AND_EVENT, (name, name, id)) 
+            data = cursor.fetchall()
+            connection.commit()
+        cursor.close()
+    formatted_matches = format_matches(name, data)
+    return add_rating_and_fed_to_matches(id, formatted_matches)
 
 def get_results_by_event(id, cursor):
     periods = get_rating_list_periods()
-    event = get_event_by_id(id, cursor)
+    event = get_event_by_id(id)
     period = check_latest_rating_list(event['start_date'], periods)
     result = calculate_result(id, cursor)
     for row in result:
@@ -128,9 +149,14 @@ def get_events_by_player(name, cursor):
     cursor.execute(EVENTS_BY_PLAYER, (name, name))
     return cursor.fetchall()
 
-def get_event_by_id(id, cursor):
-    cursor.execute(EVENT_BY_ID, (id,))
-    return cursor.fetchone()
+def get_event_by_id(id):
+    with connection:
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(EVENT_BY_ID, (id,)) 
+            event = cursor.fetchone()
+            connection.commit()
+        cursor.close()
+    return event
 
 def add_event(cursor):
     cursor.execute(INSERT_EVENT)
