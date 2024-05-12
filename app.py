aa2c8eac3173938beb59521a6be09c9100b105d4
Csaba Katona <115178933+Treev13@users.noreply.github.com>
2024-05-12 08:07:54 +0200

    move the connection to database.py

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for, json, sess
 import os
 from dotenv import load_dotenv
 from werkzeug.utils import secure_filename
-import psycopg2
-from psycopg2.extras import RealDictCursor
 import pandas as pd
-from methods import get_matches_by_player_on_event, file_upload_to_database, get_results_by_event, get_event_by_id, get_infos_by_player_on_events
-from queries import PLAYERS, MATCHES_BY_PLAYER_AND_EVENT
+from methods import get_players, get_infos_by_player_on_events, get_matches_by_player_on_event, get_results_by_event, get_event_by_id, file_upload_to_database
 
 load_dotenv()
 
@@ -19,44 +16,28 @@ app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-app.secret_key = 'This is your secret key to utilize session in Flask'
-
-connection = psycopg2.connect(database=os.getenv("DATABASE"),
-                              user=os.getenv("USER"),
-                              password=os.getenv("PASSWORD"),
-                              host=os.getenv("HOST"),
-                              port=os.getenv("PORT"))
+app.secret_key = os.getenv("APP_SECRET_KEY")
 
 @app.get('/')
-def get_players ():
-    with connection:
-        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
-            cursor.execute(PLAYERS) 
-            data = cursor.fetchall()
+def get_main_data ():
+    data = get_players()
     return render_template('index.html', data=data)
 
 @app.get('/<name>')
 def events_by_player (name):
-    with connection:
-        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
-            event_infos = get_infos_by_player_on_events(name, cursor)
-
-    return render_template('events_by_player.html', events=event_infos)
-
-@app.get('/event/<id>')
-def event_result (id):
-    with connection:
-        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
-            event = get_event_by_id(id, cursor)
-            results = get_results_by_event(id, cursor)
-    return render_template('event_result.html', results=results, event=event)
+    event_infos = get_infos_by_player_on_events(name)
+    return render_template('events_by_player.html', name=name, events=event_infos)
 
 @app.get('/<name>/<id>')
 def matches (name, id):
-    with connection:
-        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
-            data = get_matches_by_player_on_event(name, id, cursor)
+    data = get_matches_by_player_on_event(name, id)
     return render_template('matches.html', data=data)
+
+@app.get('/event/<id>')
+def event_result (id):
+    event = get_event_by_id(id)
+    results = get_results_by_event(id)
+    return render_template('event_result.html', results=results, event=event)
 
 @app.get('/upload')
 def index():
@@ -69,18 +50,14 @@ def upload_file():
 
     session['uploaded_data_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     if uploaded_file.filename != '':
-        #checked_file = check_csv_file(uploaded_file)
         uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         file_upload_to_database(connection, session)
     return render_template('uploaded.html')
 
 @app.get('/show_data')
 def show():
-    # Uploaded File Path
     data_file_path = session.get('uploaded_data_file_path', None)
-    # read csv
     uploaded_df = pd.read_csv(data_file_path, encoding='unicode_escape')
-    # Converting to html Table
     uploaded_df_html = uploaded_df.to_html()
     return render_template('show_data.html', data_var=uploaded_df_html)
 
